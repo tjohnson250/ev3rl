49e17dc7f9c1f400ac2c86da0a50ba4a0aa3942e
tjohnson250 <todd.r.johnson@uth.tmc.edu>
2018-09-03 13:20:22 -0500

    Updated to evdev2 package

--- Speak and Run Motors.py
+++ Speak and Run Motors.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-from ev3dev2.ev3 import *
+import ev3dev2.sensor.lego as sensors
+import ev3dev2.motor as motors
 import os
-from rl.agents.cem import CEMAgent
+#from rl.agents.cem import CEMAgent
+
 os.system('setfont Lat15-TerminusBold14')
-mL = LargeMotor('outB'); mL.stop_action = 'hold'
-mR = LargeMotor('outC'); mR.stop_action = 'hold'
+mL = motors.LargeMotor('outB'); ml.stop_action = 'hold'
+mR = motors.LargeMotor('outC'); mR.stop_action = 'hold'
 print('Hello, my name is EV3!')
 Sound.speak('Hello, my name is EV3!').wait()
-mL.run_to_rel_pos(position_sp= 840, speed_sp = 250)
-mR.run_to_rel_pos(position_sp=-840, speed_sp = 250)
+mL.on_to_position(10, 10)
+mR.on_to_position(10, 10)
 mL.wait_while('running')
 mR.wait_while('running')
